800b309f6d48878a4d01f0803c6503327515852f
andyxdliu0218 <andyxdliu0218@gmail.com>
2023-12-21 23:42:19 -0800

    initial commit

---
+++ appstore/constants/constants.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+    APP_INDEX = "app"
+    USER_INDEX = "user"
+    
+    STRIPE_API_KEY = "sk_test_51N5DOkIiUbeN7NmYFCRGKyZwJ0LFmG8wt8IbJJUcgwRt26tS57mCBKK7FiSBQ6BHuGARCeTM4eyW5TzvCZq3X4Ff00wQfBzf9H"
+    
+)
---
+++ appstore/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"appstore/backend"
+	"net/http"
+	"appstore/handler"	
+	"log"
+	"fmt"
+	"appstore/util"
+)
+
+func main() {
+    fmt.Println("started service")
+	config, err := util.LoadApplicationConfig("conf", "deploy.yml")
+    if err != nil {
+        panic(err)
+    }
+
+    backend.InitElasticsearchBackend(config.ElasticsearchConfig)
+    backend.InitGCSBackend(config.GCSConfig)
+	log.Fatal(http.ListenAndServe(":8080", handler.InitRouter(config.TokenConfig)))
+}
